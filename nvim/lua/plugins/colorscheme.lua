38fb045686e2458e34320f51afa666fb32547f62
Mike Veronie <mveronie@gmail.com>
2024-07-22 22:52:47 -0400

    Changes default colorsheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,82 +1,82 @@
 return {
-  "navarasu/onedark.nvim",
+  "ellisonleao/gruvbox.nvim",
   priority = 1000,
   config = function()
-    require('onedark').setup {
-      -- Main options --
-      style = 'warm', -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
-      transparent = false,  -- Show/hide background
-      term_colors = true, -- Change terminal color as per the selected theme style
-      ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
-      cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
-
-      -- toggle theme style ---
-      toggle_style_key = nil, -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
-      toggle_style_list = {'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light'}, -- List of styles to toggle between
-
-      -- Change code style ---
-      -- Options are italic, bold, underline, none
-      -- You can configure multiple style with comma separated, For e.g., keywords = 'italic,bold'
-      code_style = {
-        comments = 'italic',
-        keywords = 'none',
-        functions = 'none',
-        strings = 'none',
-        variables = 'none'
+    require("gruvbox").setup({
+      terminal_colors = true, -- add neovim terminal colors
+      undercurl = true,
+      underline = true,
+      bold = true,
+      italic = {
+        strings = true,
+        emphasis = true,
+        comments = true,
+        operators = false,
+        folds = true,
       },
-
-      -- Lualine options --
-      lualine = {
-        transparent = false, -- lualine center bar transparency
-      },
-
-      -- Custom Highlights --
-      colors = {}, -- Override default colors
-      highlights = {}, -- Override highlight groups
-
-      -- Plugins Config --
-      diagnostics = {
-        darker = true, -- darker colors for diagnostic
-        undercurl = true,   -- use undercurl instead of underline for diagnostics
-        background = true,    -- use background color for virtual text
-      },
-    }
-     vim.cmd("colorscheme onedark")
-  end
+      strikethrough = true,
+      invert_selection = false,
+      invert_signs = false,
+      invert_tabline = false,
+      invert_intend_guides = false,
+      inverse = true, -- invert background for search, diffs, statuslines and errors
+      contrast = "soft", -- can be "hard", "soft" or empty string
+      palette_overrides = {},
+      overrides = {},
+      dim_inactive = false,
+      transparent_mode = false,
+    })
+    vim.cmd("colorscheme gruvbox")
+  end,
 }
 
 -- return {
---   "ellisonleao/gruvbox.nvim",
+--   "navarasu/onedark.nvim",
 --   priority = 1000,
 --   config = function()
---     require('gruvbox').setup {
---       terminal_colors = true, -- add neovim terminal colors
---       undercurl = true,
---       underline = true,
---       bold = true,
---       italic = {
---         strings = true,
---         emphasis = true,
---         comments = true,
---         operators = false,
---         folds = true,
+--     require('onedark').setup {
+--       -- Main options --
+--       style = 'warm', -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+--       transparent = false,  -- Show/hide background
+--       term_colors = true, -- Change terminal color as per the selected theme style
+--       ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
+--       cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
+--
+--       -- toggle theme style ---
+--       toggle_style_key = nil, -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
+--       toggle_style_list = {'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light'}, -- List of styles to toggle between
+--
+--       -- Change code style ---
+--       -- Options are italic, bold, underline, none
+--       -- You can configure multiple style with comma separated, For e.g., keywords = 'italic,bold'
+--       code_style = {
+--         comments = 'italic',
+--         keywords = 'none',
+--         functions = 'none',
+--         strings = 'none',
+--         variables = 'none'
 --       },
---       strikethrough = true,
---       invert_selection = false,
---       invert_signs = false,
---       invert_tabline = false,
---       invert_intend_guides = false,
---       inverse = true, -- invert background for search, diffs, statuslines and errors
---       contrast = "", -- can be "hard", "soft" or empty string
---       palette_overrides = {},
---       overrides = {},
---       dim_inactive = false,
---       transparent_mode = false,
+--
+--       -- Lualine options --
+--       lualine = {
+--         transparent = false, -- lualine center bar transparency
+--       },
+--
+--       -- Custom Highlights --
+--       colors = {}, -- Override default colors
+--       highlights = {}, -- Override highlight groups
+--
+--       -- Plugins Config --
+--       diagnostics = {
+--         darker = true, -- darker colors for diagnostic
+--         undercurl = true,   -- use undercurl instead of underline for diagnostics
+--         background = true,    -- use background color for virtual text
+--       },
 --     }
---      vim.cmd("colorscheme gruvbox")
+--      vim.cmd("colorscheme onedark")
 --   end
 -- }
-
+--
 -- return {
 --   "folke/tokyonight.nvim",
 --   priority = 1000,
@@ -90,7 +90,7 @@ return {
 --     local fg_dark = "#B4D0E9"
 --     local fg_gutter = "#627E97"
 --     local border = "#547998"
--- 
+--
 --     require("tokyonight").setup({
 --       style = "night",
 --       on_colors = function(colors)
@@ -111,7 +111,7 @@ return {
 --         colors.fg_sidebar = fg_dark
 --       end
 --     })
--- 
+--
 --     vim.cmd("colorscheme tokyonight")
 --   end
 -- }
